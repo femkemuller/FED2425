8e1467f7bbe033c89af720cce46f0d8c219cf980
femkemuller <femke130303@gmail.com>
2024-10-01 23:11:40 +0200

    fix error

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 // JavaScript Document
 console.log("hi");
 
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var menu = document.getElementById("menu-content");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
