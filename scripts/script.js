bb7045ca34563da8cec60ae9ad792c7352658b59
femkemuller <femke130303@gmail.com>
2024-10-02 15:23:06 +0200

    JS gefixt

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,56 @@
 // JavaScript Document
 console.log("hi");
 
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    var menu = document.getElementById("menu-content");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-});
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+/* JOUW CODE HIER - stap 4 */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+var openButton = document.querySelector("nav:first-of-type > button");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+openButton.onclick = openMenu;
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {  
+  // zoek de nav op
+  var deNav = document.querySelector("nav:nth-of-type(2)");
+  // voeg class toe aan nav
+  deNav.classList.add("toonMenu");
+}
+
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+/* JOUW CODE HIER - stap 5 */
+
+// stap 1 - zoek sluiten button op
+var sluitButton = document.querySelector("nav:nth-of-type(2) button");
+
+// stap 2 - laat die button luisteren naar kliks
+sluitButton.onclick = sluitMenu;
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu() {
+  var deNav = document.querySelector("nav:nth-of-type(2)");
+  deNav.classList.remove("toonMenu");
+}
+
+/**********************************/
+/* bonus: menu sluiten met escape */
+/**********************************/
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav:nth-of-type(2)");
+    deNav.classList.remove("toonMenu");
+  }
+}
